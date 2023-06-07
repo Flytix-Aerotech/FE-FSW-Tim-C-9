874939b5890d2c724d7b48c6962c9299b066e536
INleanRe <yanawahyu37@gmail.com>
2023-06-07 17:46:17 +0700

    yanche apdet nih

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Text besar</h1>
+      <h1 className="text-3xl font-bold underline">Text nya kurang</h1>
     </div>
   );
 }
