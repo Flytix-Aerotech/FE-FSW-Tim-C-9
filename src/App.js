df957a393f22857963c8b460daf4b1e5d553ebe3
fauziibnuhkm <114082942+fauziibnuhkm@users.noreply.github.com>
2023-06-22 02:00:22 +0700

    update 22 juni

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import "./App.css";
+import './App.css';
+import './style.css';
+import React from 'react';
+import { BaseLayout } from './component/Shared/base_layout';
+import { OrderHistory } from './component/Pages/Orders/order_history';
+import Notification from './component/Pages/Notifications/notification';
+import UpdateAccount from './component/Pages/Account/update_account';
+import Example from './component/Pages/Orders/order1';
+import Approute from './config/route/approute';
+
+// eslint-disable-next-line require-jsdoc
+// function App() {
+//   return (
+//     <Approute/>
+//   );
+// }
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Text besar</h1>
-    </div>
+    <>
+      <BaseLayout>
+        {/* <div className="w-screen h-screen"> */}
+          <Approute/>
+        {/* </div> */}
+      </BaseLayout>
+    </>
   );
 }
 
