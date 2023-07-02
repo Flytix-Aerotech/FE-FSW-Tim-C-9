dfb23ed70f83aef883210ad46c69574da2c9a60d
Endang Hendayatna <91519488+endanghendayatna@users.noreply.github.com>
2023-07-02 16:35:04 +0700

    Update Arrivalnput.jsx
    Perubahan Masukkan Code -> Kota

--- src/components/custom_input/Arrivalnput.jsx
+++ src/components/custom_input/Arrivalnput.jsx
@@ -51,7 +51,7 @@ const ArrivalInput = ({ form }) => {
               </span>
               <input
                 type="text"
-                placeholder="Masukkan code"
+                placeholder="Masukkan Kota"
                 className="border text-black font-medium border-gray-300 text-sm rounded-lg py-2 pl-10 pr-2 flex-1 outline-none focus:border-purple-600"
                 value={searchQuery}
                 onChange={(e) => setSearchQuery(e.target.value)}
